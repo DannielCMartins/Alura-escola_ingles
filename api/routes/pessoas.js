a04f20a22193271e8c3efdb76db30412931d2a30
DannielCMartins <53942956+DannielCMartins@users.noreply.github.com>
2021-12-11 10:02:59 -0300

    melhorias

--- api/routes/pessoas.js
+++ api/routes/pessoas.js
@@ -5,12 +5,14 @@ const router = Router()
 
 router.get('/pessoas', PessoaController.lista)
 router.get('/pessoas/:id', PessoaController.listaPorId)
-
 router.post('/pessoas', PessoaController.criaPessoa)
-
 router.put('/pessoas/:id', PessoaController.atualizarPessoa)
-
 router.delete('/pessoas/:id', PessoaController.deletaPessoa)
+/******************Matriculas *************************/
+router.get('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.listaUmaMatricula)
+router.post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
+router.put('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.atualizaMatricula)
+router.delete('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.apagaMatricula)
 
 
 module.exports = router;
